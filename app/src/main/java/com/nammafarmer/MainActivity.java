d993a03eafc66151fdd3ca3c374796eb6c11eda2
rohini sankar <rohini.srnarayanan@gmail.com>
2020-06-15 12:09:18 +0530

    phone verification

--- app/src/main/java/com/nammafarmer/MainActivity.java
+++ app/src/main/java/com/nammafarmer/MainActivity.java
@@ -1,6 +1,61 @@
 package com.nammafarmer;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+
+
+import com.phonenumberui.PhoneNumberActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private String mobileNumber = "";
+    private Button btnVerify;
+    private static final int REQUEST_PHONE_VERIFICATION = 1080;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnVerify = findViewById(R.id.btnVerify);
+
+        btnVerify.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, PhoneNumberActivity.class);
+                //Optionally you can add toolbar title
+                intent.putExtra("TITLE", getResources().getString(R.string.app_name));
+                //Optionally you can pass phone number to populate automatically.
+                intent.putExtra("PHONE_NUMBER", "");
+                startActivityForResult(intent, REQUEST_PHONE_VERIFICATION);
+            }
+        });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_PHONE_VERIFICATION:
+// If mobile number is verified successfully then you get your phone number to perform further operations.
+                if (data != null && data.hasExtra("PHONE_NUMBER") && data.getStringExtra("PHONE_NUMBER") != null) {
+                    String phoneNumber = data.getStringExtra("PHONE_NUMBER");
+                    mobileNumber = phoneNumber;
+                } else {
+                    // If mobile number is not verified successfully You can hendle according to your requirement.
+                    Toast.makeText(MainActivity.this,getString(R.string.mobile_verification_fails),Toast.LENGTH_SHORT);
+                }
+                break;
+        }
+    }
+}
+
+
+/*import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -10,7 +65,6 @@ import android.widget.Button;
 import com.nammafarmer.seeds.ActSeedsLanding;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +78,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Intent intent = new Intent(this, ActSeedsLanding.class);
         startActivity(intent);
     }
+
+
+    */
 }
